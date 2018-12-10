3b2ef37eb02f9d895706a229692a1c13c1bc1940
Milosz Frejnik <milosz@frejnik.com>
2018-12-10 23:14:02 +0100

    Further slides

--- presentation/index.js
+++ presentation/index.js
@@ -43,6 +43,10 @@ const theme = createTheme({
   secondary: "Helvetica"
 });
 
+theme.screen.global['.spectacle-content'] = {
+  maxHeight: "unset"
+}
+
 const CustomList = styled(List)`
   line-height: 1.2;
 `;
@@ -51,6 +55,10 @@ const StyledListItem = styled(ListItem)`
   margin-bottom: 0.6em;
   list-style-position: initial;
 `;
+
+const HeadingStyled = styled(Heading)`
+  margin-bottom: 0.4em;
+`
 export default class Presentation extends React.Component {
   render() {
     return (
@@ -61,7 +69,7 @@ export default class Presentation extends React.Component {
           </Heading>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={2} textColor="secondary">Goal 🥅</Heading>
+          <HeadingStyled size={2} textColor="secondary">Goal 🥅</HeadingStyled>
           <CustomList>
             <StyledListItem>Get to know essential syntax in order to get productive.</StyledListItem>
             <StyledListItem>Recognize common baseline in the team</StyledListItem>
@@ -69,22 +77,22 @@ export default class Presentation extends React.Component {
           </CustomList>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Boilerplate Code</Heading>
-          <Link href="https://github.com/mifrej/es6-essentials" target="blank" >Starter code -> https://github.com/mifrej/es6-essentials</Link>
+          <HeadingStyled size={6} textColor="secondary" caps>Boilerplate Code</HeadingStyled>
+          <Link href="https://github.com/mifrej/es6-essentials" target="_blank" >Starter code -> https://github.com/mifrej/es6-essentials</Link>
           <Text>We will be coding it in Code Sandbox</Text>
         </Slide>
         <Slide>
-          <Heading size={2}>ES Modules</Heading>
+          <HeadingStyled size={2}>ES Modules</HeadingStyled>
           <Text>Exports</Text>
-          <CodePane lang="js" source={require('raw-loader!./assets/modules_export')} textSize="20" theme="external" contentEditable />
+          <CodePane lang="js" source={require('raw-loader!./assets/modules_export')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide>
-          <Heading size={2}>ES Modules</Heading>
+          <HeadingStyled size={2}>ES Modules</HeadingStyled>
           <Text>Imports</Text>
-          <CodePane lang="js" source={require('raw-loader!./assets/modules_import')} textSize="20" theme="external" contentEditable />
+          <CodePane lang="js" source={require('raw-loader!./assets/modules_import')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide>
-          <Heading size={2}>Exercise</Heading>
+          <HeadingStyled size={2}>Exercise</HeadingStyled>
           <CustomList ordered>
             <StyledListItem>Setup a function as <strong>default export</strong></StyledListItem>
             <StyledListItem>Setup at least <strong>2 named exports</strong></StyledListItem>
@@ -93,17 +101,17 @@ export default class Presentation extends React.Component {
           </CustomList>
         </Slide>
         <Slide transition={["fade"]}>
-          <Heading size={2} textColor="secondary">Variables</Heading>
-          <Heading size={4} textColor="secondary">Hoisting</Heading>
-          <CodePane lang="js" source={require('raw-loader!./assets/vars_hoisting')} textSize="20" theme="external" contentEditable />
+          <HeadingStyled size={2} textColor="secondary">Variables</HeadingStyled>
+          <HeadingStyled size={4} textColor="secondary">Hoisting</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/vars_hoisting')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide transition={["fade"]}>
-          <Heading size={2} textColor="secondary">Variables</Heading>
-          <Heading size={4}>New types - <strong>const</strong> and <strong>let</strong></Heading>
-          <CodePane lang="js" source={require('raw-loader!./assets/vars')} textSize="20" theme="external" contentEditable />
+          <HeadingStyled size={2} textColor="secondary">Variables</HeadingStyled>
+          <HeadingStyled size={4}>New types - <strong>const</strong> and <strong>let</strong></HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/vars')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide>
-          <Heading size={2}>Exercise</Heading>
+          <HeadingStyled size={2}>Exercise</HeadingStyled>
           <CustomList ordered>
             <StyledListItem>Convert var `name` to a const type</StyledListItem>
             <StyledListItem>Add "surname" prop to the object in `const name`</StyledListItem>
@@ -111,12 +119,12 @@ export default class Presentation extends React.Component {
           </CustomList>
         </Slide>
         <Slide>
-          <Heading size={4}>Browser native DOM selector methods</Heading>
+          <HeadingStyled size={4}>Browser native DOM selector methods</HeadingStyled>
           <Text textColor="tertiary">Where is my $ selector 😲</Text>
           <Image src="https://media.giphy.com/media/3o84U72tKO389H2lI4/giphy.gif"/>
         </Slide>
         <Slide>
-          <Heading size={4}>Native browser document methods for DOM manipulations</Heading>
+          <HeadingStyled size={4}>Native browser document methods for DOM manipulations</HeadingStyled>
           <CustomList>
             <StyledListItem><Code>getElementById</Code></StyledListItem>
             <StyledListItem><Code>querySelector</Code></StyledListItem>
@@ -126,8 +134,12 @@ export default class Presentation extends React.Component {
             <StyledListItem><Code>createElement</Code></StyledListItem>
           </CustomList>
         </Slide>
+        <Slide>
+          <HeadingStyled size={4}>Template strings</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/template_strings')} textSize="22" theme="external" contentEditable />
+        </Slide>
         <Slide bgColor="tertiary">
-          <Heading size={2}>Exercise</Heading>
+          <HeadingStyled size={2}>Exercise</HeadingStyled>
           <CustomList ordered>
             <StyledListItem>Append provided ul list to a document</StyledListItem>
             <StyledListItem>Select all nodes with <Code>[data-time]</Code> attribute</StyledListItem>
@@ -137,17 +149,41 @@ export default class Presentation extends React.Component {
           </CustomList>
         </Slide>
         <Slide>
-          <Heading size={3}>Function declaration</Heading>
-          <Heading size={5}>With Hoisting</Heading>
-          <CodePane lang="js" source={require('raw-loader!./assets/function_hoisting')} textSize="20" theme="external" contentEditable />
+          <HeadingStyled size={3}>Function declaration</HeadingStyled>
+          <HeadingStyled size={5}>With Hoisting</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/function_hoisting')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide>
-          <Heading size={3}>Function expression</Heading>
-          <CodePane lang="js" source={require('raw-loader!./assets/function_expression')} textSize="20" theme="external" contentEditable />
+          <HeadingStyled size={3}>Function expression</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/function_expression')} textSize="22" theme="external" contentEditable />
         </Slide>
         <Slide>
-          <Heading size={3}>Arrow <Code>=></Code> function</Heading>
-          <CodePane lang="js" source={require('raw-loader!./assets/arrow_function')} textSize="20" theme="external" contentEditable />
+          <HeadingStyled size={3}>Arrow <Code>=></Code> function</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/arrow_function')} textSize="22" theme="external" contentEditable />
+        </Slide>
+        <Slide bgColor="tertiary">
+          <HeadingStyled size={3}>=> Function characteristics</HeadingStyled>
+          <CustomList>
+            <StyledListItem>Does not change <Code>this</Code> context</StyledListItem>
+            <StyledListItem>Doesn't have keyword <Code>arguments</Code></StyledListItem>
+            <StyledListItem>Does not replace regular function declaration</StyledListItem>
+            <StyledListItem>Reduce code boilerplate when anonymous function required</StyledListItem>
+          </CustomList>
+        </Slide>
+        <Slide bgColor="tertiary">
+          <HeadingStyled size={2}>Exercise</HeadingStyled>
+          <CustomList ordered>
+            <StyledListItem>Rewrite ES5 functions to arrow functions from <Code>src/es6/starter/arrow_function.js</Code></StyledListItem>
+            <StyledListItem>Fix script from <Code>src/es6/starter/arrow_function_this.js</Code></StyledListItem>
+          </CustomList>
+        </Slide>
+        <Slide>
+          <HeadingStyled size={2}>Default function arguments</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/function_default_arguments')} textSize="22" theme="external" contentEditable />
+        </Slide>
+        <Slide>
+          <HeadingStyled size={2}>Object literal improvements</HeadingStyled>
+          <CodePane lang="js" source={require('raw-loader!./assets/object_literal_improvements')} textSize="22" theme="external" contentEditable />
         </Slide>
       </Deck>
     );
